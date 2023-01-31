026104cf59934ffb144f0c65867761663aef9009
matthew <permahuman@gmail.com>
2023-01-30 20:53:20 -0800

    Create contact section text for phone number address and hours open. Style these elements in the contactInfo class in the css file. Flex with color text and add our handwritten font family.

--- src/contactPage.js
+++ src/contactPage.js
@@ -19,7 +19,23 @@ function insertContactPage(){
          <li class = 'contactButton' style = 'padding:.3rem; border:5px solid black; border-radius:15px;' >Contact</li>
  
        </menu>
+
+       <main class = 'contactInfo'>
+
+       <h1> Contact us! </h1>
+       <h2> Phone Number: 123-123-1234
+       <h2> Address: Fourteen Seventeen Thirty Eight Big Drive Way suite 707</h2>
+       <h3> Open</h3>
+        <p>Friday & Saturday Lunch 11:30 - 3:30</p>
+        <p>Friday & Saturday Dinner 4:30 - 8:30</p>
+        <p>Sunday Lunch 11:30 - 3:30</p>
+        <p>Sunday Dinner 4:00 - 8:30</p>
+        <p>Monday Lunch 11:30 - 3:30</p>
+        <p>Monday 4:30 - 8:30 <p/>
+        <p>Wednesday 4:30 to 8:30</p>
+        <p>Thursday 4:30 - 8:30</p>
        
+        </main>
        
        `
      
