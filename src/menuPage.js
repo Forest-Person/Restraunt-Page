f76d27707bf990f520bc884d0ed80492ffabf902
matthew <permahuman@gmail.com>
2023-02-01 08:47:10 -0800

    Style all the menuChoices divs. Give them their own background. Flex bos with flexwrap applied, and row direction. Center everything and make responsive.

--- src/menuPage.js
+++ src/menuPage.js
@@ -6,7 +6,7 @@ function insertMenuPage(){
   const content = document.querySelector('#content');
 
 
-  content.style.cssText = 'padding: 0;margin: 0;background-image:url("./travis-grossen-chef-unsplash2.jpg");background-size: cover;background-repeat: no-repeat;background-position: center;background-attachment: fixed;height:100vh;width:100vw;border:0;'
+  content.style.cssText = 'padding: 0;margin: 0;height:100vh;width:100vw;border:0;'
 
 
     let menuPageString = 
@@ -23,23 +23,41 @@ function insertMenuPage(){
  
        </menu>
 
-       <section class = 'menuChoices'>
-          
-        <img src = "./alfredo.PNG">
-          <h2>Chicken Alfredo</h2>
-          <p> Our slow cooked tender chicken slices with long gorgeous hand made pasta strips in the most luxurious cream sauce you have ever dreamed of.</p>
-       
-        <img src = "./pizza.PNG">
-          <h2>Pizza Marguerite</h2>
-          <p>Our finest pizza with tomato, jalapenos, proscuitto, basil and arugula.</p>
-         
-        <img src = "./dessert.PNG">
-          <h2> Pistaccio Pie</h2>
-          <p>Rated best pistaccio pie by foodie Magizine! Creamy texture, sweet flavor, laden with frosted pistaccio and candied goji berries for that Health Kick to the soul!<p>
+       <section class = 'menuChoices' style = 'display:flex; flex-direction:row; flex-wrap:wrap; justify-content:center; padding:1rem; align-items:center;background-color:var(--greenTransparent);'>
 
-        <img src = "./drinks.PNG">
-          <h2> Drinks for ALL! </h2>
-          <p> Water, Beer, Lemonade, Crystal waters, glacial ice by the bucket.</p>
+       <div>
+          
+            <img src = "./alfredo.PNG">
+              <h2>Chicken Alfredo</h2>
+              <p> Our slow cooked tender chicken slices with long gorgeous hand made pasta strips in the most luxurious cream sauce you have ever dreamed of.</p>
+       
+        </div>
+              
+        <div>
+            
+            <img src = "./pizza.PNG">
+              <h2>Pizza Marguerite</h2>
+              <p>Our finest pizza with tomato, jalapenos, proscuitto, basil and arugula.</p>
+            
+        </div>
+
+        <div>
+
+            <img src = "./dessert.PNG">
+              <h2> Pistaccio Pie</h2>
+              <p>Rated best pistaccio pie by foodie Magizine! Creamy texture, sweet flavor, laden with frosted pistaccio and candied goji berries for that Health Kick to the soul!</p>
+
+        </div>
+
+        <div>
+
+            <img src = "./drinks.PNG">
+              <h2> Drinks for ALL! </h2>
+              <p> Water, Beer, Lemonade, Crystal waters, glacial ice by the bucket.</p>
+
+        </div>
+
+        </section>
         
        `
      
